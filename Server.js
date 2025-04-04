f175bad6510731b43bec80866ceb37a3ec49e39b
Q-Reine <q.wihogora@alustudent.com>
2025-04-04 15:53:20 +0200

    PORT

--- Server.js
+++ Server.js
@@ -8,7 +8,7 @@ import bodyParser from 'body-parser';
 
 
 dotenv.config();
-const port =process.env.PORT||5000
+const port =process.env.PORT||3000
 const db_user =process.env.DB_USER;
 const db_name =process.env.DB_NAME;
 const db_pass =process.env.DB_PASS;
